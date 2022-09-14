eee3197b8825c8a3b39bcfadc27c8e32e4553fc2
ElisaCarballo <elisa.carballo79@gmail.com>
2022-09-13 21:35:02 -0500

    clase

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import RefDom from './components/RefDom'
+import SimpleForm from './components/SimpleForm'
+import FormHook from './components/FormHook'
+
+function App () {
+  return (
+    <div className='App'>
+      <FormHook />
+    </div>
+  )
+}
+
+export default App
